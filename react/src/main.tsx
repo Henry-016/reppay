d713ecd12a9beed5884ba88e8bb180f280db9d24
danielmendes23 <daniel.mendes@arapiraca.ufal.br>
2026-06-08 18:59:51 -0300

    correção no body

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.scss'
-import './pages/telas/global.scss'
 import Cadastro from './pages/telas/Cadastro.js'
 import Login from './pages/telas/Login.js'
 import HomeGeral from './pages/telas/HomeGeral'
